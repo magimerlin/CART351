eea49224fec9f1660ceee963088c14faee9426a6
Magi Merlin <magimerlin@magis-mbp.wireless.concordia.ca>
2018-10-29 20:45:11 -0400

    work updates!

---
+++ PHP-weekseven/phpex.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>Hello world example in php</title>
+</head>
+<body>
+<?php
+echo "Hello World!";
+?>
+<html>
+<head>
+<title>Hello world example in php</title>
+<style>
+div
+{
+  width: 200px;
+  height: 200px;
+  margin: 2px;
+  padding: 4px;
+  border-width:2px;
+  border-style:solid;
+  border-color:#ccc;
+  background-color:#98bf21;
+  font-size:14px;
+  color:#111111;
+  font-family:arial;
+
+}
+</style>
+</head>
+<body>
+<?php
+echo "<div> Hello World in div tags </div>";
+echo "<br> <h2> hello world in the header tags </br></h2>
+echo "<div id = "whatever'> Hello world in div tags using a <strong>
+?>
+</body>
+</html>
+</body>
+</html>
